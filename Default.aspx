db7f43a11d96fe368218353d3d5dabc4ac25fc6a
Troy Howard <thoward37@gmail.com>
2013-09-03 05:35:55 -0700

    initial commit

---
+++ Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="AppfogHelloWorldApp.Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.2.1/pure-min.css" />
+    <link rel="stylesheet" type="text/css" href="css/site.css" />
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  </style>
+</head>
+<body>
+  <div class="pure-g-r">
+    <div class="pure-u-1-4"></div>
+    <div class="pure-u-1-2">
+    <div class="content">
+      <div id="clouds_blue"><img src="img/clouds_blue.png"></div>
+
+      <h1>Hello World</h1>
+      <p>Welcome to Appfog.</p>
+      <hr />
+      
+      <div>
+        <h2>Try It!</h2>
+        <p>Type something in the text box and update the label by doing a post back server side.</p>
+        <form id="form1" class="pure-form" runat="server">
+          <h4>
+            <asp:Label class="content-quote" ID="testLabel" runat="server" Text="Label"></asp:Label>
+          </h4>
+          <asp:TextBox runat="server" ID="txtSetLabel"></asp:TextBox>
+          <asp:Button class="pure-button" ID="btnChangeLabel" runat="server" Text="Change Text" OnClick="btnChangeLabel_Click" />
+        </form>
+      </div>
+      <hr />
+      
+      <div>
+        <h2>Application Settings</h2>
+        <asp:Panel ID="settingsPanel" runat="server"></asp:Panel>
+      </div>
+      
+    </div>
+ 
+  </div>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Specialized;
+//using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Web.Configuration;
+
+namespace AppfogHelloWorldApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Title = "ASP.NET 2.0 App";
+            testLabel.Text = "ASP.Net 2.0 App";
+
+            NameValueCollection appSettings = WebConfigurationManager.AppSettings as NameValueCollection;
+            foreach (String key in appSettings.AllKeys)
+            {
+                /*
+                TableRow row = new TableRow();
+                
+                TableCell keyCell = new TableCell();
+                TableCell valueCell = new TableCell();
+                
+                keyCell.Text = key;
+                valueCell.Text = appSettings[key];
+                
+                row.Cells.Add(keyCell);
+                row.Cells.Add(valueCell);
+                
+                settingsTable.Rows.Add(row);
+                */
+  
+                HtmlGenericControl keyHeader = new HtmlGenericControl("h4");
+                keyHeader.InnerHtml = key;
+
+                HtmlGenericControl valuePre = new HtmlGenericControl("pre");
+
+                HtmlGenericControl valueCode = new HtmlGenericControl("code");
+                valueCode.InnerHtml = appSettings[key];
+                valuePre.Controls.Add(valueCode);
+                
+                settingsPanel.Controls.Add(keyHeader);
+                settingsPanel.Controls.Add(valuePre);
+            }        
+        }
+
+        protected void btnChangeLabel_Click(object sender, EventArgs e)
+        {
+            testLabel.Text = String.IsNullOrEmpty(txtSetLabel.Text) ? "NOTHING" : txtSetLabel.Text;
+        }
+    }
+}
